e09a50ff0952a86152d8a8c37333b1dcfbf93996
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-11 21:55:28 +0530

    Create template for home page in page-template folder

---
+++ page-templates/template-home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * Template part for displaying home page.
+ *
+ * @package DesignFly
+ */
+// Exit if accessed directly.
+  defined( 'ABSPATH' ) || exit;
+
+  get_header();
+?>
+
+<?php
+  get_footer();
+?>
