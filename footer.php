86b6c77afa21e792d2d38620af26e7331ec201b1
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-12 11:33:21 +0530

    Create footer for the site

--- footer.php
+++ footer.php
@@ -13,17 +13,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'design-fly' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'design-fly' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
+			<p class="footer-text"><span>&#169;</span> 2020 - D'SIGNfly<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'design-fly' ), 'design-fly', '<a href="https://google.com/">Adwait</a>' );
+				printf( esc_html__( '%1$s by %2$s.', 'design-fly' ), 'Designed', '<a href="https://google.com/">Adwait</a>' );
 				?>
+			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
