75582b5bfcd834990c29bf942fea651d632495e9
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-12 21:16:20 +0530

    Add portfolio home section on the home page

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -15,12 +15,30 @@
   <?php
 	if( is_front_page() ) {
 	?>
+  <!-- Code for displaying home banner. -->
 	<div class="intro-container" style="background-image: url( <?php esc_attr_e( get_theme_file_uri( '/assets/images/full-slider.png' ) ); ?> );">
 		<div class="intro-content">
 			<h1 class="intro-heading"><?php esc_html( the_title() ); ?></h1>
 			<div class="intro-info"><?php esc_html( the_content() ); ?></div>
 		</div>
 	</div>
+
+  <!-- Code for displaying custom posts from Portfolio post type. -->
+  <?php
+  $args = array(
+    'post_type'     => 'df-portfolio',
+    'post_per_page' => 6
+  );
+
+  $query = new WP_Query( $args );
+  ?>
+  <div class="portfolio-home">
+    <div class="portfolio-hometop">
+      <p class="portfolio-hometop__title"><?php esc_html_e( 'D\'SIGN IS THE SOUL' ); ?></p>
+      <button class="portfolio-hometop__btn" type="button">View All</button>
+    </div>
+    <hr/>
+  </div>
 	<?php
 	}
 	?>
