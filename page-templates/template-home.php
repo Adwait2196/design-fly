f08b20479ca71a4bbb13024d6c884eabdbd60069
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-12 16:33:17 +0530

    Add home banner to home's template page

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -10,8 +10,16 @@
   defined( 'ABSPATH' ) || exit;
 
   get_header();
-?>
 
-<?php
+  if( is_front_page() ) {
+  ?>
+    <div class="intro-container" style="background-image: url( <?php esc_attr_e( get_theme_file_uri( '/assets/images/full-slider.png' ) ); ?> );">
+      <div class="intro-content">
+        <h1 class="intro-heading"><?php esc_html( the_title() ); ?></h1>
+        <div class="intro-info"><?php esc_html( the_content() ); ?></div>
+      </div>
+    </div>
+  <?php
+  }
   get_footer();
 ?>
