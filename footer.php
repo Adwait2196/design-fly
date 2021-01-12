79e5b40f4b526e2cdb1cc1cfa2f8dd5783270be0
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-12 16:30:36 +0530

    Add welcome and contact us section in the footer

--- footer.php
+++ footer.php
@@ -13,12 +13,24 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p class="footer-text"><span>&#169;</span> 2020 - D'SIGNfly<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( '%1$s by %2$s.', 'design-fly' ), 'Designed', '<a href="https://google.com/">Adwait</a>' );
-				?>
-			</p>
+			<div class="site-contactinfo">
+					<div class="welcome-container">
+						<h2>Welcome to D'SIGNfly</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+					<div class="contactus-container">
+						<h2>Contact Us</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</div>
+			</div>
+			<div class="site-copyright">
+				<p class="footer-text"><span>&#169;</span> 2020 - D'SIGNfly<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( '%1$s by %2$s.', 'design-fly' ), 'Designed', '<a href="https://google.com/">Adwait</a>' );
+					?>
+				</p>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
