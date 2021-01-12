22995fee6b146bb89525cfaeb975446c31c819d0
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-12 18:14:41 +0530

    Move banner code from template-home to header file

--- header.php
+++ header.php
@@ -60,3 +60,16 @@
 			</form>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
+
+	<?php
+	if( is_front_page() ) {
+	?>
+	<div class="intro-container" style="background-image: url( <?php esc_attr_e( get_theme_file_uri( '/assets/images/full-slider.png' ) ); ?> );">
+		<div class="intro-content">
+			<h1 class="intro-heading"><?php esc_html( the_title() ); ?></h1>
+			<div class="intro-info"><?php esc_html( the_content() ); ?></div>
+		</div>
+	</div>
+	<?php
+	}
+	?>
