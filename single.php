2eeee0765b062b59d3c24bc7ad06bd60cb1cfaa9
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-13 18:25:40 +0530

    Edit single.php

--- single.php
+++ single.php
@@ -11,30 +11,39 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<div class="post-structure">
+			<div class="post-maincontent">
+				<?php
+				while ( have_posts() ) :
+					the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'design-fly' ) . '</span> <span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'design-fly' ) . '</span> <span class="nav-title">%title</span>',
+						)
+					);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'design-fly' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'design-fly' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) : ?>
+						<div class="comments-wrapper">
+							<hr/>
+							<p class="bars"><?php esc_html_e( 'Comments', 'desgin-fly' ); ?></p>
+							<hr/>
+						</div>
+						<?php comments_template();
+					endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+				endwhile; // End of the loop.
+				?>
+			</div>
+			<div class="post-sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
 	</main><!-- #main -->
-
+	<hr/>
 <?php
-get_sidebar();
 get_footer();
