8fec6a902e26d96d051289fa13bd897185cb681a
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-13 21:35:03 +0530

    Move banner section from home template file to header file

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -15,14 +15,6 @@
   <?php
 	if( is_front_page() ) {
 	?>
-  <!-- Code for displaying home banner. -->
-	<div class="intro-container" style="background-image: url( <?php esc_attr_e( get_theme_file_uri( '/assets/images/full-slider.png' ) ); ?> );">
-		<div class="intro-content">
-			<h1 class="intro-heading"><?php esc_html( the_title() ); ?></h1>
-			<div class="intro-info"><?php esc_html( the_content() ); ?></div>
-		</div>
-	</div>
-
   <!-- Code for displaying custom posts from Portfolio post type. -->
   <?php
   $args = array(
