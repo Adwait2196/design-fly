8529848efa1aebc96cc0d633dc8cc0bbd9842075
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-13 10:55:50 +0530

    Add grid section to display posts from portfolio post type

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -35,10 +35,35 @@
   <div class="portfolio-home">
     <div class="portfolio-hometop">
       <p class="portfolio-hometop__title"><?php esc_html_e( 'D\'SIGN IS THE SOUL' ); ?></p>
-      <button class="portfolio-hometop__btn" type="button">View All</button>
+      <a class="portfolio-hometop__btn" href="<?php esc_attr_e( get_permalink( '#' ) ) ?>">
+        <span class="portfolio-hometop__btntxt"><?php esc_html_e( 'View All', 'design-fly' ); ?></span>
+      </a>
     </div>
     <hr/>
+    <div class="portfolio-images__container">
+    <?php
+      if( $query -> have_posts() ) {
+        while( $query -> have_posts() ) {
+          $query -> the_post();
+          echo '<a href="'.esc_attr( get_permalink() ).'" rel="lightbox">';
+          echo '<img class="portfolio-home__images" src="'. get_the_post_thumbnail_url() .'">';
+          echo '</a>';
+        }
+    ?>
   </div>
+  </div>
+  <?php
+      }
+      else {
+        ?>
+        <div class="container">
+          <p>
+            <?php esc_html_e( 'Sorry, no portfolio items found. Please add some portfolio items in admin dashboard.', 'design-fly' ); ?>
+          </p>
+        </div>
+        <?php
+      }
+    ?>
 	<?php
 	}
 	?>
