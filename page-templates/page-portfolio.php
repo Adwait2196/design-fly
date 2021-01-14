3d02c382c09b79dde7b28dbf6f5c21c84b24ee60
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-14 14:43:00 +0530

    Create portfolio template page

--- page-templates/page-portfolio.php
+++ page-templates/page-portfolio.php
@@ -11,46 +11,45 @@
 get_header();
 
 $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-$query = new WP_Query( array(
-  'post_type'       => 'df-portfolio',
-  'posts_per_page'  => 15,
-  'paged'           => $paged
-) );
+$args = array(
+  'post_type'     => 'df-portfolio',
+  'posts_per_page' => 15,
+  'paged'         => $paged
+);
 
-if( $query -> have_posts() ) {
-  ?>
-    <div class="view-image-container">
-      <div class="content-wrapper">
-        <img src="" />
-        <span class="close"><span class="dashicons dashicons-no-alt"></span></span>
+$portfolio_query = new WP_Query ( $args );
+?>
+  <div class="portfolio-container">
+    <div class="portfolio-header">
+      <h2 class="portfolio-header__heading"><span class="portfolio-heading__text">DESIGN IS THE SOUL</span></h2>
+      <div class="portfolio-header__buttons">
+        <button type="button" class="portfolio-header__button">Advertising</button>
+        <button type="button" class="portfolio-header__button">Multimedia</button>
+        <button type="button" class="portfolio-header__button">Photography</button>
       </div>
     </div>
-
-    <div class="portfolio-wrapper">
-      <div class="portfolio-wrapper-top">
-        <p class="title">
-          <?php esc_html( 'Sign is the soul' ); ?>
-          <hr/>
-        </p>
-      </div>
+    <hr class="horizontal-bar1"/>
+    <div class="portfolio-body">
       <?php
-      while ($query -> have_posts() ) {
-        $query -> the_post();
-        get_template_part( 'template-parts/content', 'df-portfolio' );
-      }
+        if( $portfolio_query -> have_posts() ) {
+          while( $portfolio_query -> have_posts() ) {
+            $portfolio_query -> the_post();
+            echo '<a href="'.esc_attr( get_the_post_thumbnail_url() ).'" rel="lightbox">';
+            echo '<img class="portfolio-images" src="'. get_the_post_thumbnail_url() .'"/>';
+            echo '</a>';
+          }
+        }
+        else {
+          ?>
+          <p>
+            <?php esc_html_e( 'Sorry, no portfolio items found. Please add some portfolio items in admin dashboard.', 'design-fly' ); ?>
+          </p>
+          <?php
+        }
       ?>
     </div>
-    <?php
-      //design_fly_pagination_bar( $query );
-    ?>
-  <?php
-}
-else {
-  ?>
-    <p>
-      <?php _e( 'Sorry, no portfolio items found', 'design-fly' ); ?>
-    </p>
-  <?php
-}
-
+    <?php design_fly_pagination_bar ( $portfolio_query ); ?>
+  </div>
+  <hr class="horizontal-bar2"/>
+<?php
 get_footer();
