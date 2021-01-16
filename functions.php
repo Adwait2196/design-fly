f999a3d3bc776a9caf8c62e497b7b708f016af72
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-16 22:10:10 +0530

    Add function to add portfolio post type in pre get posts

--- functions.php
+++ functions.php
@@ -198,3 +198,10 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 */
 require get_template_directory() . '/inc/custom-post-type-function.php';
 add_action( 'init', 'designfly_custom_post_type' );
+
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'df-portfolio' ) );
+    return $query;
+}
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
