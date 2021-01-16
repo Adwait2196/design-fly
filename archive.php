714055a96c0605800c64c578756b395645909498
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-16 22:11:55 +0530

    Minor changes

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-
+<div class="blogs-container">
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -31,7 +31,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content', 'blog-template' );
 
 			endwhile;
 
@@ -45,7 +45,9 @@ get_header();
 		?>
 
 	</main><!-- #main -->
-
+	<div class="blogs-sidebar">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
 <?php
-get_sidebar();
 get_footer();
