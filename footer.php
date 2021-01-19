cb7c9e328ec450d5bce77fd2d5337fdc3d0944f6
Adwait Deshpande <adwait.deshpande21@gmail.com>
2021-01-19 20:32:21 +0530

    Minor Change

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 			</div>
 			<div class="contactus-container">
 				<h2>Contact Us</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				<div class="contactus-icons">
 					<?php
 						$icons_arr = [ 'facebook', 'gp', 'linkedin', 'pin', 'twitter' ];
